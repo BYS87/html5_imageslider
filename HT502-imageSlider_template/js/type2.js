ee93a4b17c4693ca6d006fd773bcc558eee8fbd5
BYS87 <bysvictoria@naver.com>
2019-01-20 16:07:39 +0900

    type2.js jquery 수정

--- HT502-imageSlider_template/js/type2.js
+++ HT502-imageSlider_template/js/type2.js
@@ -7,11 +7,18 @@ $(document).ready(function() {
     // $("#slider ul li:nth-child(3)").hide();
 
     setInterval(function() {
-       $("#slider ul li:first-of-type").fadeOut();
-       $("#slider ul li:nth-child(2)").fadeIn();
-       //html의 li 순서를 바꾸어서 위명령문들 반복 수행.
-       $("#slider ul").append($("#slider ul li:first-of-type"));
-    }, 2000);
+        $("#slider ul li:first-of-type")
+        .fadeOut(2000)
+        .next()
+        .fadeIn(1000)
+        .end()
+        .appendTo($("#slider ul"));
+
+    //    $("#slider ul li:first-of-type").fadeOut(2000);
+    //    $("#slider ul li:nth-child(2)").fadeIn(1000);
+    //    //html의 li 순서를 바꾸어서 위명령문들 반복 수행.
+    //    $("#slider ul").append($("#slider ul li:first-of-type"));
+    }, 3000);
 
 
 });
